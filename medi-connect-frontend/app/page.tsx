f4f9081c8bbc559c47333fdb12724a801040261b
Mohsin-Pix <mohsin.h@pixcodex.com>
2026-09-07 15:00:05 +0500

    Configure the tanstack query and create the general axios instance

--- medi-connect-frontend/app/page.tsx
+++ medi-connect-frontend/app/page.tsx
@@ -1,5 +1,6 @@
+'use client'
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -62,6 +63,15 @@ export default function Home() {
           >
             Documentation
           </a>
+          <Button
+            className="md:w-[158px]"
+            onClick={() => {
+              alert("You clicked the button!");
+            }}
+            variant="secondary"
+          >
+            Click Me
+          </Button>
         </div>
       </main>
     </div>
