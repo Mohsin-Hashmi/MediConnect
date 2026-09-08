68a3714b69f582ec8e944c0326fa6899778b3f88
Mohsin-Pix <mohsin.h@pixcodex.com>
2026-09-08 11:32:59 +0500

    setup the frontend codebase

--- medi-connect-frontend/providers/query-provider.tsx
+++ medi-connect-frontend/providers/query-provider.tsx
@@ -1,16 +1,10 @@
 "use client";
 
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-export function QueryProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export function QueryProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
     () =>
       new QueryClient({
@@ -21,12 +15,13 @@ export function QueryProvider({
             refetchOnWindowFocus: false,
           },
         },
-      })
+      }),
   );
 
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
